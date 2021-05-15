93b49bd7ce395c2035df1d036daad83a67a9c691
growvv <2092876368@qq.com>
2021-05-15 09:57:28 +0800

    增加一些全连接层

--- Simple-GAN/simple_gan.py
+++ Simple-GAN/simple_gan.py
@@ -11,9 +11,11 @@ class Discriminator(nn.Module):
     def __init__(self, in_features):
         super().__init__()
         self.disc = nn.Sequential(
-            nn.Linear(in_features, 128),  # 输入in_features 748， 748 -> 128
-            nn.LeakyReLU(0.01),  # 
-            nn.Linear(128, 1), # 128 -> 1
+            nn.Linear(in_features, 256),  # 784 -> 256
+            nn.LeakyReLU(0.2),  # 
+            nn.Linear(256, 256), # 256 -> 256
+            nn.LeakyReLU(0.2),
+            nn.Linear(256, 1),  # 255 -> 1
             nn.Sigmoid(),   # 将实数映射到[0,1]区间
         )
 
@@ -25,9 +27,11 @@ class Generator(nn.Module):
     def __init__(self, z_dim, image_dim):
         super().__init__()
         self.gen = nn.Sequential(
-            nn.Linear(z_dim, 256),   # z_dim维 升至 256维
-            nn.LeakyReLU(0.01),
-            nn.Linear(256, image_dim), # 256维 升至 image_dim维度
+            nn.Linear(z_dim, 256),   # 64 升至 256维
+            nn.ReLU(True),
+            nn.Linear(256, 256),   # 256 -> 256
+            nn.ReLU(True),
+            nn.Linear(256, image_dim), # 256 -> 784
             nn.Tanh(),  # Tanh使得生成数据范围在[-1, 1]，因为真实数据经过transforms后也是在这个区间
         )
 
@@ -93,7 +97,7 @@ for epoch in range(num_epochs):
 
             print(
                 f"Epoch [{epoch}/{num_epochs}] Batch {batch_idx}/{len(loader)}' \
-                    Loss D: {lossD:.4f}, loss G: {lossG:.4f}"
+                    loss D: {lossD:.4f}, loss G: {lossG:.4f}"
             )
 
             with torch.no_grad():
